a2b12ea230da84478974a0087fe3ed1575ef004a
Arya Kumar <2019akumar2@tjhsst.edu>
2016-12-03 10:37:47 -0500

    Completed init script

--- init.py
+++ init.py
@@ -1,3 +1,26 @@
 import os
+import sys
+import subprocess
+
 dirs = [d for d in os.listdir('.') if (os.path.isdir(os.path.join('.', d)) and os.path.basename(os.path.join('.', d)).startswith("+"))]
-print str(dirs)
+
+print("Welcome to the install script!\n")
+print("Here are all valid devices:")
+for dev in range(0, len(dirs)):
+	print(str(dev+1) + ") " + str(dirs[dev])[1:])
+
+def pickDevice():
+	user = input("\nEnter a number from 1 to " + str(len(dirs)) + ". (-1 to quit): ")
+	if(int(user) == -1):
+		print("Quitting")
+		sys.exit()
+	elif(int(user) < 1 or int(user) > len(dirs)):
+		print("Invalid Input:")
+		return pickDevice()
+	else:
+		return dirs[int(user)-1]
+
+chosenDir = pickDevice()
+print("\nDevice \"" + str(chosenDir)[1:] + "\" chosen.")
+
+subprocess.call(["python", "master.py"], cwd=os.path.abspath(os.path.join('.', chosenDir, "Setup")))
