3917ee3176c0786ac9fd86b75e6c0b75b79d5cd4
Arya Kumar <2019akumar2@tjhsst.edu>
2016-12-14 15:52:45 -0500

    Changing conf

--- +DummyDevice/Setup/master.py
+++ +DummyDevice/Setup/master.py
@@ -1,4 +1,5 @@
 import os
 print("\nRunning dummy device script!")
 os.system('sudo apt-get install avahi-daemon -y')
-os.system('sudo systemctl enable avahi-daemon; sudo systemctl start avahi-daemon')
+os.system('sudo systemctl enable avahi-daemon; sudo systemctl stop avahi-daemon')
+os.system('rm /etc/avahi/avahi-daemon.conf && cp ./avahi-daemon.conf /etc/avahi/avahi-daemon.conf')
