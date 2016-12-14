bc218625d6f162aa64e8fe2dbdb03d7a2bae44f5
Arya Kumar <2019akumar2@tjhsst.edu>
2016-12-14 06:55:52 -0500

    Added systemctl enabling

--- +DummyDevice/Setup/master.py
+++ +DummyDevice/Setup/master.py
@@ -1,4 +1,4 @@
 import os
 print("\nRunning dummy device script!")
 os.system('sudo apt-get install avahi-daemon -y')
-
+os.system('sudo systemctl enable avahi-daemon; sudo systemctl start avahi-daemon')
