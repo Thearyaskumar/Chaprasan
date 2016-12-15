1ebd70f772ec361c1e8ee353661a9893924705df
Arya Kumar <2019akumar2@tjhsst.edu>
2016-12-15 13:20:20 -0500

    changing config

--- +DummyDevice/Setup/master.py
+++ +DummyDevice/Setup/master.py
@@ -1,21 +1,24 @@
 import os
 
-print("\n\n---Installing Avahi---")
+print("\n\n---Installing Avahi---\n\n")
 os.system('sudo apt-get install avahi-daemon -y')
 os.system('sudo systemctl enable avahi-daemon; sudo systemctl stop avahi-daemon')
 
-print("\n\n---Configuring installation file---")
+print("\n\n---Configuring installation file---\n\n")
 DeviceName = os.popen('cd .. && basename "$PWD"').read()[1:].lower()
 
-#Remove existing config:
+print('Removing existing config...')
 os.system('rm -Rf /etc/avahi/* && cp ./avahi-daemon.conf /etc/avahi/')
 
-#Editing the config:
+print('Reading the config...')
 with open('/etc/avahi/avahi-daemon.conf', 'r') as file:
     data = file.readlines()
 
 data[21] = 'host-name=' + DeviceName
 
-# and write everything back
+print('Writing everything back...')
 with open('/etc/avahi/avahi-daemon.conf', 'w') as file:
     file.writelines( data )
+
+print('\n\n---Restarting Avahi---\n\n')
+os.system('sudo systemctl start avahi-daemon')
